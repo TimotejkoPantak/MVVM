d2dce990c720c531d1e183c153c0f55c036d30b9
TimotejkoPantak <timopantakgit@gmail.com>
2021-01-11 21:44:21 +0100

    Add project files.

---
+++ MVVM/Views/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace MVVM.Views
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
